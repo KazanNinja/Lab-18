6253e414fca6dec711bfabba1119f8bb332c6684
KazanNinja <TheKazanNinja@Gmail.com>
2016-11-29 09:05:04 -0800

    Finished Card.java - added exceptions, getters, setters, constructors, toString

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,42 @@
 
 public class Card {
+
+    private String rank;
+    private String suit;
+
+    public Card(String rank, String suit) {
+        if (Integer.parseInt(rank) >= 11 || Integer.parseInt(rank) <= 1) {
+            throw new RuntimeException("The rank must be from 2-10");
+            //Throws a runtime exception because the rank is too high
+        }
+
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank() {
+
+        return rank;
+    }
+
+    public int getRankInt() {
+        //Checks if the rank is from 2-10 if not returns -1
+        if (Integer.parseInt(rank) >= 11 || Integer.parseInt(rank) <= 1) {
+            return Integer.parseInt(rank);
+        }
+        return -1;
+    }
+
+    public String getSuit() {
+
+        return suit;
+    }
+
+    public String toString() {
+        return "Card{" +
+                ", rank='" + rank + '\'' +
+                ", suit='" + suit + '\'' +
+                '}';
+    }
+
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,7 @@
 
 public class Deck {
+
+    public static void main(String[] args) {
+        Card card = new Card("1", "Hearts");
+    }
 }
